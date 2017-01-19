e6511a6ad1c5fd5e67728445617ef98110a71eb8
Mario Paolini <trepaolini@gmail.com>
2017-01-19 15:20:15 -0800

    Complete Class Excercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,17 +3,21 @@
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
 
+seahawks.points <- c(10,20,30,40,30,20,10,20,30)
+
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+seahawks.allowed <- c(5,10,15,20,15,10,5,10,15)
 
 # Combine your two vectors into a dataframe
-
-
+scores <- data.frame(seahawks.points,seahawks.allowed)
+View(scores)
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
+diff <- seahawks.points - seahawks.allowed
+scores <- c(scores,diff)
 
 
 # Create a new column "won" which is TRUE if the Seahawks won
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,16 +1,17 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
-# This will produce a data frame called `USPersonalExpenditure`
-
+# This will produce a data frame called `USPersonalExpenditure'
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
+is.data.frame("USPersonalExpenditure")
 
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+my.frame <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
 
@@ -18,21 +19,23 @@
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
+
 # What are the row names of your dataframe?
 
 
 # Create a column "category" that is equal to your rownames
-
+my.frame$category <- row.names(my.frame)
 
 # How much money was spent on personal care in 1940?
-
+my.frame[4,]
+sum(my.frame[4,2:5])
 
 # How much money was spent on Food and Tobacco in 1960?
-
+sum(my.frame[1,2:5])
 
 # What was the highest expenditure category in 1960?
 # Hint: use the `max()` function to find the largest, then compare that to the column
-
+my.frame[my.frame$X1960 == max(my.frame$X1960), "category"]
 
 # Define a function `DetectHighest` that takes in a year as a parameter, and
 # returns the highest spending category of that year
